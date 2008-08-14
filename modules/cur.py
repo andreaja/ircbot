0a538860a4ff9ff7020899ac4ffc0aa6af5ed162
idimmu <idimmu@faith.(none)>
2008-08-14 11:55:34 +0100

    currency calculator module actually works now

--- modules/cur.py
+++ modules/cur.py
@@ -8,12 +8,13 @@ Licensed under the unicorn license.
 http://inamidst.com/phenny/
 """
 
-import urllib
+import re, urllib
 from lxml import etree
 
 def cur(phenny, input):
+	a, b, c =  input.group(2).split()
 	XE_URL = 'http://www.xe.com/tmi/xe-output.php?appid=ffxtn-1.0&from_amount=%s&from=%s&to=%s'
-	url = XE_URL % (input.groups()[1], input.groups()[2], input.groups()[3])
+	url = XE_URL % (a, b, c)
 	dom = etree.parse(urllib.urlopen(url))
 	if dom.xpath('/xe-output/headers/status')[0].text == '0':
 		phenny.say('%s %s is %s %s' %(dom.xpath('/xe-output/conversion/from-amount')[0].text, dom.xpath('/xe-output/conversion/from-currency-name')[0].text, dom.xpath('/xe-output/conversion/converted-amount')[0].text, dom.xpath('/xe-output/conversion/to-currency-name')[0].text))
@@ -22,6 +23,7 @@ def cur(phenny, input):
 
 cur.commands = ['cur']
 cur.priority = 'high'
+cur.example = '.cur 1 usd gbp'
 
 if __name__ == '__main__':
 	print __doc__.strip()
